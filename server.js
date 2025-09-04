e32f84bef2783b25f3b4827a9811867971bb7a82
omar-gaal <omargaal123@gmail.com>
2025-09-04 13:08:48 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express, { json } from "express";
+
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Node.js Messages REST API 🚀 UPDATED!");
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
